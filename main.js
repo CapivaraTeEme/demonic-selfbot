b1618d19fc4b735843c03468140e978b9d6e0716
CapivaraTeEme <juliocesaralmeydacostaa@gmail.com>
2024-10-28 14:29:41 -0500

    Main

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+// No skiddeen, ya los conosco mogolicos de mierda
 const { Client } = require('discord.js-selfbot-v13');
 const readline = require('readline');
 
-// Banner ASCII
-console.log(`
+(async () => {
+    const chalk = (await import('chalk')).default;
+
+    console.log(chalk.magentaBright(`
                                                                                                     
                                                                                                     
                                                                                                     
@@ -10,11 +13,11 @@ console.log(`
                         @@@@@                                           @@@@@                       
                     @@@@@@@@                                              @@@@@@@                   
                 @@@@@@@@@@                    @       @                   @@@@@@@@@@@               
-             @@@@@@@@@@@@@                    @@     @@                    @@@@@@@@@@@@@            
-           @@@@@@@@@@@@@@@@                  @@@@   @@@@                  @@@@@@@@@@@@@@@@          
+             @@@@@@@@@@@@@                    @@     @@                    @@@@@@@@@@@@@            By CapivaraTeEme
+           @@@@@@@@@@@@@@@@                  @@@@   @@@@                  @@@@@@@@@@@@@@@@          & Exta$is/Saox
           @@@@@@@@@@@@@@@@@@@                @@@@@@@@@@@                @@@@@@@@@@@@@@@@@@@         
-        @@@@@@@@@@@@@@@@@@@@@@@@@@          @@@@@@@@@@@@            @@@@@@@@@@@@@@@@@@@@@@@@@       
-      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@     
+        @@@@@@@@@@@@@@@@@@@@@@@@@@          @@@@@@@@@@@@            @@@@@@@@@@@@@@@@@@@@@@@@@          For:
+      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@     discord.gg/demonicc
      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@     
     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    
    @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   
@@ -31,157 +34,170 @@ console.log(`
                                                  @@                                                 
                                                                                                     
                                             DEMONIC S3LFB0T
-`);
+    `));
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
 
-let token;
+    let token;
 
-function askToken() {
-    rl.question('Put your token > ', async (input) => {
-        token = input.trim();
-        const client = new Client();
+    function askToken() {
+        rl.question(chalk.magentaBright('Put your token > '), async (input) => {
+            token = input.trim();
+            const client = new Client();
 
-        try {
-            await client.login(token);
-            console.log('[+] Logged in successfully!');
+            try {
+                await client.login(token);
+                console.log('[+] Logged in successfully!');
+                showMenu(client);
+            } catch (err) {
+                console.error('[x] ERROR: Invalid token or login failed');
+                process.exit(1);
+            }
+        });
+    }
+
+    function showMenu(client) {
+        console.log(`
+     ______________________________________________________________
+    |  (1) Nuke             (2) Raid                    (3) Bypass |
+    |                          (4) Spam                            |
+    |                   (5)  createchannels                        |
+    ---------------------------------------------------------------
+        `);
+        rl.question('> ', (option) => {
+            switch (option.trim()) {
+                case '1':
+                    handleNuke(client);
+                    break;
+                case '2':
+                    handleRaid(client);
+                    break;
+                case '3':
+                    handleBypass(client);
+                    break;
+                case '4':
+                    handleSpam(client);
+                    break;
+                case '5':
+                    handleCreateChannels(client);
+                    break;
+                default:
+                    console.log('[x] Invalid option. Returning to menu...');
+                    showMenu(client);
+            }
+        });
+    }
+
+    function handleNuke(client) {
+        rl.question('Put Server ID > ', async (serverId) => {
+            try {
+                const name_c = 'destroyed-by-demonicselfbot';
+                const guild = await client.guilds.fetch(serverId.trim());
+                await Promise.all(guild.channels.cache.map((channel) => channel.delete()));
+                await Promise.all(guild.emojis.cache.map((emoji) => emoji.delete()));
+                await guild.channels.create(name_c, { type: 'GUILD_TEXT' });
+                console.log('[+] Server nuked successfully!');
+            } catch {
+                console.log('[x] ERROR: No tienes permisos en este servidor');
+            }
             showMenu(client);
-        } catch (err) {
-            console.error('[x] ERROR: Invalid token or login failed');
-            process.exit(1);
-        }
-    });
-}
+        });
+    }
 
-function showMenu(client) {
-    console.log(`
-     ____________________________________________________
-    |  (1) Nuke             (2) Raid                    (3) Bypass  |
-    |                          (4) Spam                                       |
-    |                   (5)  createchannels                              |
-    -------------------------------------------------------
-    `);
-    rl.question('> ', (option) => {
-        switch (option.trim()) {
-            case '1':
-                handleNuke(client);
-                break;
-            case '2':
-                handleRaid(client);
-                break;
-            case '3':
-                handleBypass(client);
-                break;
-            case '4':
-                handleSpam(client);
-                break;
-            case '5':
-                handleCreateChannels(client);
-                break;
-            default:
-                console.log('[x] Invalid option. Returning to menu...');
-                showMenu(client);
-        }
-    });
-}
+    function handleRaid(client) {
+        rl.question('Put Server ID > ', async (serverId) => {
+            rl.question('Put Message to Spam > ', (message) => {
+                rl.question('Put Channel Names > ', async (channelName) => {
+                    try {
+                        const webhookname = 'Destroyed by DemonicS3LF';
+                        const guild = await client.guilds.fetch(serverId.trim());
 
-function handleNuke(client) {
-    rl.question('Put Server ID > ', async (serverId) => {
-        try {
-            const guild = await client.guilds.fetch(serverId.trim());
-            await Promise.all(guild.channels.cache.map((channel) => channel.delete()));
-            await Promise.all(guild.emojis.cache.map((emoji) => emoji.delete()));
-            await guild.channels.create('get-nuked', { type: 'GUILD_TEXT' });
-            console.log('[+] Server nuked successfully!');
-        } catch {
-            console.log('[x] ERROR: No tienes permisos en este servidor');
-        }
-        showMenu(client);
-    });
-}
-
-function handleRaid(client) {
-    rl.question('Put Server ID > ', async (serverId) => {
-        rl.question('Put Message to Spam > ', (message) => {
-            rl.question('Put Channel Names > ', async (channelName) => {
-                try {
-                    const guild = await client.guilds.fetch(serverId.trim());
-                    for (let i = 0; i < 100; i++) {
-                        const channel = await guild.channels.create(channelName, { type: 'GUILD_TEXT' });
-                        const webhook = await channel.createWebhook('Destroyed by DemonicSelfbot');
-                        for (let j = 0; j < 100; j++) {
-                            await webhook.send(message);
+                        const webhooks = [];
+                        for (let i = 0; i < 100; i++) {
+                            const channel = await guild.channels.create(channelName, { type: 'GUILD_TEXT' });
+                            const webhook = await channel.createWebhook(webhookname);
+                            webhooks.push(webhook);
                         }
+
+                        console.log('[+] Se han creado canales y webhooks.');
+
+                        for (const webhook of webhooks) {
+                            for (let j = 0; j < 100; j++) {
+                                await webhook.send(message);
+                            }
+                        }
+
+                        console.log('[+] Raid completed!');
+                    } catch (error) {
+                        console.log('[x] ERROR: No tienes permisos en este servidor');
                     }
-                    console.log('[+] Raid completed!');
-                } catch {
-                    console.log('[x] ERROR: No tienes permisos en este servidor');
-                }
-                showMenu(client);
+                    showMenu(client);
+                });
             });
         });
-    });
-}
+    }
 
-function handleBypass(client) {
-    rl.question('Put Server ID > ', async (serverId) => {
-        rl.question('Put Name of the Channels > ', (channelName) => {
+    function handleBypass(client) {
+        rl.question('Put Server ID > ', async (serverId) => {
+            rl.question('Put Name of the Channels > ', (channelName) => {
+                rl.question('Put Message to Spam > ', async (message) => {
+                    try {
+                        const guild = await client.guilds.fetch(serverId.trim());
+                        await Promise.all(guild.channels.cache.map((ch) => ch.setName(channelName)));
+                        for (const channel of guild.channels.cache.values()) {
+                            const webhook = await channel.createWebhook('Destroyed by DemonicSelfbot');
+                            await webhook.send(message);
+                        }
+                        console.log('[+] Bypass completed!');
+                    } catch {
+                        console.log('[x] ERROR: No tienes permisos en este servidor');
+                    }
+                    showMenu(client);
+                });
+            });
+        });
+    }
+
+    function handleSpam(client) {
+        rl.question('Put Server ID > ', async (serverId) => {
             rl.question('Put Message to Spam > ', async (message) => {
                 try {
                     const guild = await client.guilds.fetch(serverId.trim());
-                    await Promise.all(guild.channels.cache.map((ch) => ch.setName(channelName)));
                     for (const channel of guild.channels.cache.values()) {
-                        const webhook = await channel.createWebhook('Destroyed by DemonicSelfbot');
-                        await webhook.send(message);
+                        const webhook = await channel.createWebhook('Destroyed By DemonicSelfbot');
+                        for (let i = 0; i < 100; i++) {
+                            await webhook.send(message);
+                        }
                     }
-                    console.log('[+] Bypass completed!');
+                    console.log('[+] Spam completed!');
                 } catch {
                     console.log('[x] ERROR: No tienes permisos en este servidor');
                 }
                 showMenu(client);
             });
         });
-    });
-}
+    }
 
-function handleSpam(client) {
-    rl.question('Put Server ID > ', async (serverId) => {
-        rl.question('Put Message to Spam > ', async (message) => {
-            try {
-                const guild = await client.guilds.fetch(serverId.trim());
-                for (const channel of guild.channels.cache.values()) {
-                    const webhook = await channel.createWebhook('Destroyed By DemonicSelfbot');
+    function handleCreateChannels(client) {
+        rl.question('Put Server ID > ', async (serverId) => {
+            rl.question('Put Name of the Channels > ', async (channelName) => {
+                try {
+                    const guild = await client.guilds.fetch(serverId.trim());
                     for (let i = 0; i < 100; i++) {
-                        await webhook.send(message);
+                        await guild.channels.create(channelName, { type: 'GUILD_TEXT' });
                     }
+                    console.log('[+] Channels created!');
+                } catch {
+                    console.log('[x] ERROR: No tienes permisos en este servidor');
                 }
-                console.log('[+] Spam completed!');
-            } catch {
-                console.log('[x] ERROR: No tienes permisos en este servidor');
-            }
-            showMenu(client);
+                showMenu(client);
+            });
         });
-    });
-}
+    }
 
-function handleCreateChannels(client) {
-    rl.question('Put Server ID > ', async (serverId) => {
-        rl.question('Put Name of the Channels > ', async (channelName) => {
-            try {
-                const guild = await client.guilds.fetch(serverId.trim());
-                for (let i = 0; i < 100; i++) {
-                    await guild.channels.create(channelName, { type: 'GUILD_TEXT' });
-                }
-                console.log('[+] Channels created!');
-            } catch {
-                console.log('[x] ERROR: No tienes permisos en este servidor');
-            }
-            showMenu(client);
-        });
-    });
-}
+    askToken();
+})();
 
-askToken();
