8742378742e58f04d50e3c5a278817040f149762
CapivaraTeEme <juliocesaralmeydacostaa@gmail.com>
2024-10-28 07:16:39 -0500

    Install

---
+++ install.bat
@@ -0,0 +1,5 @@
+@echo off
+echo                                                                Installing Dependences
+npm install discord.js-selfbot-v13
+echo                                                                Wait...
+pause
